e62231f1b74df8142e5fe048c2a447a3f992efad
djuarezgf <46350150+djuarezgf@users.noreply.github.com>
2022-11-01 21:19:13 +0100

    Initial commit

---
+++ src/main/java/de/samply/teiler/TeilerApplication.java
@@ -0,0 +1,13 @@
+package de.samply.teiler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeilerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeilerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/samply/teiler/TeilerApplicationTests.java
@@ -0,0 +1,13 @@
+package de.samply.teiler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
