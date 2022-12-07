ed47e443455a5491b279015222ec33d1b4686b61
juarez <d.juarez@dkfz-heidelberg.de>
2022-12-07 20:40:14 +0100

    Add LDM Client

---
+++ src/main/java/de/samply/blaze/LdmClient.java
@@ -0,0 +1,10 @@
+package de.samply.blaze;
+
+public class LdmClient {
+
+  public String retrieve (String source, String query, String outputTemplate, String outputFormat){
+    //TODO
+    return null;
+  }
+
+}
